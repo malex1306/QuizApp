4a3b34f3e2b2e86741a52243ebd4b467f1add489
malex1306 <alexandremarcel350@gmail.com>
2024-08-27 15:42:26 +0200

    add cards

---
+++ screens/addCards.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AddCards = () => {
+  return (
+    <View>
+      <Text>addCards</Text>
+    </View>
+  )
+}
+
+export default AddCards
+
+const styles = StyleSheet.create({})
