84a7782efe639213811a4477e7bc32314a285ef1
malex1306 <alexandremarcel350@gmail.com>
2024-08-31 12:06:15 +0200

    new site

---
+++ screens/profil.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const profil = () => {
+  return (
+    <View>
+      <Text>profil</Text>
+    </View>
+  )
+}
+
+export default profil
+
+const styles = StyleSheet.create({})
